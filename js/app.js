8429ec6225bca628c65477e734dd304e9ad5b28c
Wes <wesmarberry@gmail.com>
2019-04-01 15:17:02 -0500

    Created tamagatchi constructor in tamagatchi class

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+//class for newborn tamagatchi
+class Tamagatchi {
+	constructor(){
+		this.age = 0
+		this.hunger = 5
+		this.sleepiness = 5
+		this.boredom = 5
+		this.name = prompt("What is your Tamagatchi's name?")
+	}
+}
